9efdd1566337b604d44a6dcbe747493b9080e7e3
kiks2627 <94597953+kiks2627@users.noreply.github.com>
2024-12-11 12:59:25 +0100

    Update version.go

--- go/vt/servenv/version.go
+++ go/vt/servenv/version.go
@@ -1,7 +1,7 @@
 package servenv
 
 // versionName holds the current version of the application.
-const versionName = "1.2.3"
+const versionName = "1.2.7"
 
 // GetVersion returns the current version of the application.
 func GetVersion() string {
